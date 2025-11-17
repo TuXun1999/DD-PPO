c7c35a60f6b94b85363604f716976915cf3439e1
Xun Tu <tu000080@cs-u-rpm-srv-01.cs.umn.edu>
2025-11-17 15:21:41 -0600

    Update evaluate objects function

--- evaluation_benchmark/evaluate_objects.sh
+++ evaluation_benchmark/evaluate_objects.sh
@@ -1,7 +1,7 @@
-directories=../collected_models
+directories=../collected_demos
 
 for dir in "$directories"/*; do
     dir=$(basename ${dir})
     echo "$dir"
-    python3 move_cube.py --env $dir --rollout_num 20 -r
+    python3 move_cube.py --env $dir -d $directories --rollout_num 20 -r
 done
